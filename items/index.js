de1f2860df19917cb132e229b76ff145eaeb1032
Yuki Xu <yuuki@Yukis-MacBook-Pro.local>
2020-12-08 23:32:11 -0500

    image changes

--- items/index.js
+++ items/index.js
@@ -1,5 +1,5 @@
-export default [{"id": "item1", "fname":"item1.md", "specialImage":"/img/Mcouat_750x500.png"},
-{"id": "item2", "fname":"item2.md", "specialImage":"/img/AchieveMore750x500.png"},
-{"id": "item3", "fname":"item3.md", "specialImage":"/img/YearbookCover750x500.png"},
-{"id": "item4", "fname":"item4.md", "specialImage":"/img/SkillsCompeition750x500.png"},
-{"id": "item5", "fname":"item5.md", "specialImage":"/img/Piano750x500.png"}];
+export default [{"id": "item1", "fname":"item1.md", "specialImage":"/img/CourseRegistration500px.png"},
+{"id": "item2", "fname":"item2.md", "specialImage":"/img/LaptopStand500px.png"},
+{"id": "item3", "fname":"item3.md", "specialImage":"/img/Mcouat_750x500.png"},
+{"id": "item4", "fname":"item4.md", "specialImage":"/img/YearbookCover750x500.png"},
+{"id": "item5", "fname":"item5.md", "specialImage":"/img/SkillsCompeition750x500.png"}];
