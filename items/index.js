3544cfed5e70abbde70e9756991c8f62695919f5
Yuki Xu <yuuki@Yukis-MacBook-Pro.local>
2020-12-20 18:22:16 -0500

    img alt tags

--- items/index.js
+++ items/index.js
@@ -1,5 +1,5 @@
-export default [{"id": "item1", "fname":"item1.md", "specialImage":"/img/CourseRegistration500px.png"},
-{"id": "item2", "fname":"item2.md", "specialImage":"/img/LaptopStand500px.png"},
-{"id": "item3", "fname":"item3.md", "specialImage":"/img/Mcouat_750x500.png"},
-{"id": "item4", "fname":"item4.md", "specialImage":"/img/YearbookCover750x500.png"},
-{"id": "item5", "fname":"item5.md", "specialImage":"/img/SkillsCompeition750x500.png"}];
+export default [{"id": "item1", "fname":"item1.md", "specialImage":"/img/CourseRegistration500px.png", "altText": "revised laurier course registration page"},
+{"id": "item2", "fname":"item2.md", "specialImage":"/img/LaptopStand500px.png", "altText": "laptop stand"},
+{"id": "item3", "fname":"item3.md", "specialImage":"/img/Mcouat_750x500.png", "altText": "McOuat Partnership website"},
+{"id": "item4", "fname":"item4.md", "specialImage":"/img/YearbookCover750x500.png", "altText": "UHS yearbook cover"},
+{"id": "item5", "fname":"item5.md", "specialImage":"/img/SkillsCompeition750x500.png", "altText": "skills competition medal"}];
